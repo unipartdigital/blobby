8d9a45f53af350690bf10084569bab16160119cb
Ben Steadman <steadmanben1@gmail.com>
2020-03-30 16:50:37 +0100

    DELETE EVERYTHING

--- blobby/__init__.py
+++
@@ -1,6 +0,0 @@
-from .client import StorageClient, StorageError
-
-__all__ = (
-    'StorageClient',
-    'StorageError',
-)
--- ci.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-PACKAGE=blobby
-
-flake8 $PACKAGE
-pycodestyle $PACKAGE
-mypy --strict $PACKAGE
-#pylint $PACKAGE
-pytest --cov-report=term-missing --cov=$PACKAGE
